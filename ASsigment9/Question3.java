d262a22e770f2097e454c08b78b556f65504cbe4
unknown <Mandar2107>
2024-05-01 17:26:44 +0530

    Assigment9_3 done

---
+++ ASsigment9/Question3.java
@@ -0,0 +1,21 @@
+import java.util.Iterator;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String parragraphString = "The class Strin g includes methods for examining individual characters of the sequence "
+				+"for comparing strings, for searching strings, for extracting substrings, and for creating a copy of a string"+
+				" with all characters translated to uppercase or to lowercase. Case mapping is based on the Unicode"
+				+"Standard version specified by the Character class Mandar sanag"
+				+ "afagahha";
+		 
+		 String [] arr = parragraphString.split(" ");
+		 System.out.println(arr.length);
+
+
+	}
+
+}
